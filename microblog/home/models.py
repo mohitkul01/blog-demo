acd9638b460732e0ce5d89f439926b5cf29de1a9
mohitkul <mohitkul@cybage.com>
2025-07-04 20:04:11 +0530

    adding indices to homepage WIP

--- microblog/home/models.py
+++ microblog/home/models.py
@@ -3,6 +3,8 @@ from django.db import models
 from wagtail.models import Page
 from wagtail.fields import RichTextField
 from wagtail.admin.panels import FieldPanel  # Import FieldPanel
+from blog.models import BlogIndexPage
+
 
 class HomePage(Page):
     body = RichTextField(blank=True)
@@ -10,3 +12,9 @@ class HomePage(Page):
     content_panels = Page.content_panels + [
         FieldPanel('body'),
     ]
+
+    def get_context(self, request):
+        context = super().get_context(request)
+        # Fetch live BlogIndexPage children
+        context['blog_indices'] = BlogIndexPage.objects.child_of(self).live()
+        return context
